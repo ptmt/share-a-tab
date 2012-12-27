e367791b0bf0e410df9ed6864fd65b46c0805f76
Dmitry <unknownliveid@hotmail.com>
2012-12-26 21:21:10 -0500

    logo&leveldb wrapper

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var io = require('socket.io');
+var levelup = require('levelup')
 
 var app = express()
   , server = require('http').createServer(app)
 
 server = server.listen(process.env.PORT);
 io = io.listen(server);
+
+app.use(express.cookieParser());
+app.use(express.session({
+    secret: "skjghskdjfhbqigohqdiouk"
+}));
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -17,8 +23,9 @@ app.get('/client_dummy.html', function(req, res){
 
 io.sockets.on('connection', function (socket) {
     
-  socket.on('set nickname', function (name) {
-    socket.set('nickname', name, function () { socket.emit('ready'); });
+  socket.on('set location', function (href) {
+      saveHref(href);
+      socket.set('location', href, function () { socket.emit('ready'); });
   });
 
   socket.on('msg', function () {
@@ -27,3 +34,21 @@ io.sockets.on('connection', function (socket) {
     });
   });
 });
+
+var options = { createIfMissing: true, errorIfExists: false }
+var db = levelup('./mydb', options)
+
+var saveHref = function () {
+    // 2) put a key & value
+    db.put('name', 'LevelUP', function (err) {
+      if (err) return console.log('Ooops!', err) // some kind of I/O error
+    
+      // 3) fetch by key
+      db.get('name', function (err, value) {
+        if (err) return console.log('Ooops!', err) // likely the key was not found
+    
+        // ta da!
+        console.log('name=' + value)
+      })
+    })
+}
