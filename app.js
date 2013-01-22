c85c4496eebcde600b0f82e39c4fea43c8b5bea3
Dmitry <unknownliveid@hotmail.com>
2013-01-22 03:10:08 -0500

    fallback transport

--- app.js
+++ app.js
@@ -79,7 +79,20 @@ app.get('/oauth2callback',
     res.redirect('/');
   });
 
-//io.set('transports', [ 'websocket']);
+//io.configure('production', function(){
+//    console.log("Server in production mode");
+    io.enable('browser client minification');  // send minified client
+    io.enable('browser client etag');          // apply etag caching logic based on version number
+    io.enable('browser client gzip');          // gzip the file
+ //   io.set('log level', 1);                    // reduce logging
+    io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+     //   'websocket'
+    //    , 'flashsocket'
+          'htmlfile'
+        , 'xhr-polling'
+        , 'jsonp-polling'
+    ]);
+//});
 
 io.sockets.on('connection', function (socket) {
    
